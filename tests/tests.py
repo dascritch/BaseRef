ffc36989fb7fa24cd490cb1a351bf91cbac4bdd5
dascritch <xavier@dascritch.com>
2014-07-13 18:28:48 +0200

    some jokes in python are superior to javascript ones

--- tests/tests.py
+++ tests/tests.py
@@ -3,22 +3,52 @@ import unittest, io, re, json
 specifications = json.load( open('specifications.json') )
 
 class BaseRef():
-    valids = '0123456789ACDEFGHJKMNPQRTUVWX'
-    reg = re.compile(r'^['+valids+']*$')
+    glyphs = '0123456789ACDEFGHJKMNPQRTUVWX'
+    base = len(glyphs)
+    reg = re.compile(r'^['+glyphs+']*$')
+
     def isValid(self, chain) :
         if ( self.reg.match(chain) == None ) :
             return  False
         else :
             return True
-    def encode(self, numeric) :
-        return numeric
+
+    def encode(self, b10) :
+        out = ''
+        try:
+            b10 = int(b10, 10)
+        except ValueError:
+            return False
+        if (b10 < 0) or (b10 != round(b10)) :
+            return False
+        if (b10 >= self.base) :
+            out = self.encode( b10 / self.base )
+            b10 = b10 % self.base
+        out += self.glyphs[b10];
+        return out
+
+    def decode(self, based, multiplicande = 0) :
+        if self.isValid(based) == False :
+            # self.exception('not a valid coded-number');
+            return False
+        numeric = 0;
+        if ( multiplicande == None ) :
+             multiplicande = 0
+        if (len(based) > 1) :
+            numeric += self.decode( based.substr(0,based.length-1) , multiplicande+1 ) * Math.pow( this.base, multiplicande );
+            based = based.substr(-1);
+        numeric += this.glyphs.indexOf( based );
+        return numeric;
 #
 
 class BaseRefTests(unittest.TestCase):
     def test(self):
         baseref = BaseRef()
-        for parameter, expects in specifications['isValid'].items() :
-            self.assertEqual( baseref.isValid(parameter), expects)
+        for func_name , tests in specifications.items() :
+            print(func_name)
+            for parameter, expects in tests.items() :
+                print(parameter, ' -> ', expects)
+                self.assertEqual( getattr(baseref,func_name)(parameter), expects)
 
 if __name__ == '__main__':
     unittest.main()
