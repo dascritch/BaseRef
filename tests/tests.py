9154a5cd3bc64cde42185e707a07bbca95ca6930
dascritch <xavier@dascritch.com>
2014-07-13 17:39:38 +0200

    first validations in py

--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,24 @@
-import unittest, io, json
+import unittest, io, re, json
 
 specifications = json.load( open('specifications.json') )
 
-
-#
 class BaseRef():
-	def encode(numeric) :
-		return numeric
+    valids = '0123456789ACDEFGHJKMNPQRTUVWX'
+    reg = re.compile(r'^['+valids+']*$')
+    def isValid(self, chain) :
+        if ( self.reg.match(chain) == None ) :
+            return  False
+        else :
+            return True
+    def encode(self, numeric) :
+        return numeric
 #
 
 class BaseRefTests(unittest.TestCase):
     def test(self):
-    	
-        self.assertEqual( BaseRef.encode(3), 3)
+        baseref = BaseRef()
+        for parameter, expects in specifications['isValid'].items() :
+            self.assertEqual( baseref.isValid(parameter), expects)
 
 if __name__ == '__main__':
     unittest.main()
