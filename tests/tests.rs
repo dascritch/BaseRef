b36fda9699368741ba1a028c021685be56a55ffe
dascritch <xavier@dascritch.com>
2014-08-10 17:55:09 +0200

    rust : je reprends

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,18 +1,38 @@
+extern crate regex;
+use regex::Regex;
+
 /*
 
  #[path="../baseref.rs"]
 mod baseref;
 */
 
+#[allow(non_snake_case_functions)]
+fn isValid(validate: &str) -> bool {
+	let glyphs = "0123456789ACDEHFJKMNPQRTUVWX";
+	let rule = Regex::new(r"^[0123456789ACDEHFJKMNPQRTUVWX]*$");
 
+	return rule.is_match(validate);
+}
 
+#[test]
+#[allow(non_snake_case_functions)]
+fn this_tests_isValid() {
+	assert!(isValid(""));
+	assert!(isValid("0"));
+	assert!(isValid("9"));
+	assert!(isValid("A"));
+	assert!(isValid("X"));
+	assert_eq!(isValid("_"), false);
+}
 
-
+/*
 #[test]
 fn this_tests_code() {
 
 	let baseref = BaseRef();
-	//baseref.isValid();
-
+	baseref.isValid();
     //assert!(x == 2);
 }
+
+*/
